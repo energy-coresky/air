48e0e96f37b97095323a160af761c60775e020b5
Energy <71418770+energy-coresky@users.noreply.github.com>
2021-09-26 11:12:30 +0300

    set new version

--- sky.php
+++ sky.php
@@ -293,7 +293,7 @@ class SKY implements PARADISE
         define('TPL_META',   '<meta name="%s" content="%s" />');
     }
 
-    const CORE = '0.116 2021-05-31T07:01:11+03:00 energy';
+    const CORE = '0.117 2021-09-26T11:11:11+03:00 energy';
 
     static function version() {
         global $sky;
