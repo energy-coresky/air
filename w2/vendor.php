e0f2fa37c05d0772789cbdb67e300ef36e0eb176
Energy <71418770+energy-coresky@users.noreply.github.com>
2022-09-03 14:25:25 +0300

    Vendors tuned

--- w2/vendor.php
+++ w2/vendor.php
@@ -5,12 +5,20 @@ class Vendor
     function __construct() {
     }
 
+    function c_exec() {
+        exec("composer $_POST[s] 2>&1", $output, $return);
+        echo implode("\n", $output);
+        return true;
+    }
+
     function c_detail($name = false) {
         $return = $name or $name = $_POST['n'];
             //$one = file_get_contents("https://packagist.org/packages/$name.json");
         $response = file_get_contents("https://repo.packagist.org/p2/$name.json");
         $tags = function ($a) {
-            return '<div class="tags">' . implode('</div><div class="tags">', $a) . '</div>';
+            return implode('', array_map(function ($v) {
+                return '<div class="tags" onclick="sky.d.vend(1,\'' . $v . '\')">' . "$v</div>";
+            }, $a));
         };
         $authors = function ($authors) {
             return implode('<br>', array_map(function ($v) {
@@ -24,11 +32,13 @@ class Vendor
                 return $s;
             }, $authors));
         };
+        //composer require {{$act_name}}
         $json = ['html' => view('_vend.detail', [
             'act_name' => $name,
             'cnt' => $cnt = count($list = unjson($response)->packages->$name),
             'row' => $last = $list[0] ?? [],
             'detail' => print_r($last, 1),
+            'composer' => ($last && 'project' != $last->type ? "require " : "create-project ") . $name,
             'ver' => $last ? ($cnt > 1 ? ', ' . $list[1]->version . ($cnt > 2 ? ' ..' : '') : '') : '',
             'authors' => $last ? $authors($last->authors) : '-',
         ]), 'tags' => $last ? $tags($last->keywords) : ''];
@@ -38,11 +48,15 @@ class Vendor
     function c_search() {
         SKY::d('vend_s', $s = $_POST['s'] ?? 'coresky');
         $q = urlencode($s) . '&page=' . ($p = $_POST['p']);
+        $name = $tag = '';
         if ('all' != $_POST['t'])
             $q .= "&type=$_POST[t]";
+        if ('' != $_POST['g']) {
+            $q .= "&tags=$_POST[g]";
+            $tag = " tag is: $_POST[g]" . ' <input type="button" value="hide" onclick="sky.d.vend(1,1)"/>';
+        }
         $response = file_get_contents('https://packagist.org/search.json?per_page=100&q=' . $q);
         $std = unjson($response);
-        $name = '';
         $total = 'Found: 0';
         if ($std->total) {
             $name = $std->results[0]->name;
@@ -52,7 +66,7 @@ class Vendor
                 $total .= ' <input type="button" value="next" onclick="sky.d.vend(' . (1 + $p) . ')"/>';
         }
         json([
-            'total' => $total,
+            'total' => $total . $tag,
             'next' => $std->next ?? 0,
             'packages' => view('_vend.packages', [
                 'act_name' => $name,
@@ -69,7 +83,7 @@ class Vendor
         global $sky;
         return [
             'obj' => $this,
-            'types' => ['all', 'library', 'project', 'metapackage', 'composer-plugin'],
+            'types' => ['all', 'library', 'project', 'metapackage', 'composer-plugin', 'symfony-bundle'],
         ];
     }
 }
