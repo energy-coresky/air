d0ff49a96a95ad54e38c2e1c6c339412ec5335c3
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-08-27 22:27:29 +0300

    tiny fix

--- w2/zml.php
+++ w2/zml.php
@@ -5,16 +5,34 @@ class ZML # universal descriptor
     const version = 0.001;
 
     static $zml;
+    static $custom = [];
 
-    private $file;
+    private $fh;
     private $pos;
+    private $bang = [];
 
-    function __construct() {
+    function __construct($fn = false) {
         self::$zml or self::$zml = yml('+ @inc(zml)');
+        if ($fn)
+            $this->open($fn);
     }
 
-    function file($name) {
-        
+    function open($fn) {
+        $this->fh = fopen($fn, "c+b");
+        $head = fread($this->fh, 8192);
+        if ($this->pos = strpos($head, "\n\n"))
+            $this->bang = bang(substr($head, 0, $this->pos));
+    }
+
+    function close() {
+        fclose($this->fh);
+    }
+
+    function info() {
+        while (!feof($this->fh)) {
+            $contents = fread($this->fh, 8192);
+        }
+        return $this->bang;
     }
 
 }
