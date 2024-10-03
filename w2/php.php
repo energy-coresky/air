80929f7c86b0ac72b8a58a210faf9fea184f1f52
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-10-03 17:37:50 +0300

    tiny fix

--- w2/php.php
+++ w2/php.php
@@ -2,7 +2,8 @@
 
 class PHP
 {
-    const version = 0.121;
+    const version = 0.202;
+
     const KEYWORD = 1;
     const USAGE = 2;
     const DEFINITION = 3;
@@ -12,18 +13,13 @@ class PHP
     static $php;
 
     public $in;
-    public $array = [];
+    public $lines = 1;
     public $pad; # 0 for minified PHP
+    public $top;
 
-    private $stack;
-    private $y = [
-        'tok' => 0,
-        'str' => '',
-        'line' => 1,
-        'sz' => 0,
-        'rank' => 0, # KEYWORD/CHARS/USAGE/DEFINITION
-        'oc' => 0, # open/none/close 1/0/-1
-    ];
+    private $ns = '';
+    private $use = [];
+    private $stack = [];
     private $oc = [
         '{' => '}',
         '(' => ')',
@@ -33,7 +29,6 @@ class PHP
     function __construct(string $in = '', $pad = 4) {
         self::$php or self::$php = yml('+ @object@inc(php)');
         $this->in = unl($in);
-        $this->stack = [];
         $this->pad = $pad;
     }
 
@@ -42,87 +37,88 @@ class PHP
     }
 
     function trace($i, $t) {
-        $t->tok = $t->tok . ' ' . token_name($t->tok);
-        return "===================== $i\n" . var_export($t, 1);
+        $s = $t->tok . ' ' . token_name($t->tok) . ' ' . $t->str;
+        if ($t->sz)
+            $s .= " ------------------- SZ: $t->sz";
+        return "===================== $i\n$s\n";// . var_export($t, 1);
     }
 
     function __toString() {
-        $this->array or $this->parse();
-        [$lines] = array_shift($this->array);
-        $out = $prev = $trace = '';
-        foreach ($this->array as $i => $t) {
-$trace .= $this->trace($i, $t);
+        $this->top or $this->parse();
+        $out = $trace = '';
+        $el = $this->top;
+     $i=1;
+        do {
+$trace .= $this->trace($i++, $el);
             if (!$this->pad) {
-                $next = isset($this->array[$i + 1]) ? $this->array[$i + 1]->tok : '';
-                if (T_COMMENT == $t->tok
-                    || T_WHITESPACE == $t->tok && in_array($prev, self::$php->prev_space)
-                    || T_WHITESPACE == $t->tok && in_array($next, self::$php->after_space)
+                if (T_COMMENT == $el->tok
+                    || T_WHITESPACE == $el->tok && in_array($el->prev->tok, self::$php->prev_space)
+                    || T_WHITESPACE == $el->tok && in_array($el->next->tok, self::$php->after_space)
                 )
                     continue;
             }
-            $out .= $t->str;
-            $prev = $t->str;
-        }
-        return $out . "\nlines: $lines\n$trace";
+            $out .= $el->str;
+        } while ($el = $el->next);
+        return $out . "\nlines: $this->lines\n$trace";
     }
 
-    function char($y, $i) {
-        $ary =& $this->stack;
-        if (in_array($y->str, array_keys($this->oc))) {
-            $y->oc = 1;
-            $ary[] = [$y->str, $i, 1];
-        } elseif (in_array($y->str, array_values($this->oc))) {
-            $y->oc = -1;
-            if ($y->str == $this->oc[end($ary)[0]]) { // checking!!
-                $pop = array_pop($ary);
-                if ($ary) {
-                    $last = array_key_last($ary);
-                    $ary[$last][2] += $pop[2] - 1;
+    function char($n) {
+        $stk =& $this->stack;
+        if (in_array($n->str, array_keys($this->oc))) {
+            $n->oc = $n->sz = 1;
+            $stk[] = $n;
+        } elseif (in_array($n->str, array_values($this->oc))) {
+            $n->oc = -1;
+            if ($n->str == $this->oc[end($stk)->str]) { // checking!!
+                $pos = array_pop($stk);
+                if ($stk) {
+                    $last = array_key_last($stk);
+                    $stk[$last]->sz += $pos->sz - 1;
                 }
-                return $pop;
             }
         }
     }
 
-    function tokens($y = false) {
-        $y or $y = (object)$this->y;
-        $prev = false;
-        $ary =& $this->stack;
-        foreach (token_get_all($this->in) as $i => $t) {
-            [$y->tok, $y->str] = is_array($t) ? $t : [0, $t];
-            $y->oc = $y->sz = $y->rank = 0;
-            if ($ary) {
-                $last = array_key_last($ary);
-                $ary[$last][2] += strlen(' ' == $y->str ? ' ' : trim($y->str));
+    function tokens() {
+        $cur = null;
+        $stk =& $this->stack;
+        foreach (token_get_all($this->in) as $i => $tok) {
+            $n = $this->tok($tok, $cur);
+            if ($stk) {
+                $last = array_key_last($stk);
+                $stk[$last]->sz += strlen(' ' == $n->str ? ' ' : trim($n->str));
             }
-            if (1 == strlen($y->str))
-                $y->sz = $this->char($y, 1 + $i);
-            if (in_array($y->str, self::$php->keywords)) { // 2do: chk
-                $y->rank = PHP::KEYWORD;
-            } elseif (in_array($y->str, self::$php->types)) {
-                $y->rank = PHP::TYPE;
+            if (1 == strlen($n->str))
+                $this->char($n);
+            if (T_STRING == $n->tok) {
+                
+            } elseif (in_array($n->str, self::$php->keywords)) { // 2do: chk
+                $n->rank = PHP::KEYWORD;
+            } elseif (in_array($n->str, self::$php->types)) {
+                $n->rank = PHP::TYPE;
             }
-            if ($prev) {
-                if (T_COMMENT == $prev->tok && "\n" == $prev->str[-1] && T_WHITESPACE == $y->tok) {
-                    $prev->str = substr($prev->str, 0, -1);
-                    $y->str = "\n" . $y->str;
-                    $y->line--;
+            if ($cur) {
+                $cur->next = $n;
+                if (T_COMMENT == $cur->tok && "\n" == $cur->str[-1] && T_WHITESPACE == $n->tok) {
+                    $cur->str = substr($cur->str, 0, -1);
+                    $n->str = "\n" . $n->str;
+                    //$n->nl++;     $cur->nl--;
                 }
-                yield $i => $prev;
+                yield $i => $cur;
+            } else {
+                $this->top = $n;
             }
-            $prev = clone $y;
-            $y->line += substr_count($y->str, "\n");
+            $cur = $n;
         }
-        yield ++$i => $prev;
-        return $y->line;
+        yield ++$i => $cur;
     }
 
     function parse() {
-        $this->array = [[]];
-        $tokens = $this->tokens();
-        foreach ($tokens as $i => $y) {
-            switch ($y->tok) {
-                case T_NAMESPACE: ;
+        foreach ($this->tokens() as $i => $cur) {
+            switch ($cur->tok) {
+                case T_NAMESPACE:
+                    $this->ns = '';
+                    break;
                 case T_EVAL: ;
                 case T_GLOBAL: ;
                 case T_CONST: ;
@@ -130,13 +126,21 @@ $trace .= $this->trace($i, $t);
                 case T_INTERFACE: ;
                 case T_TRAIT: ;
             }
-            if ($y->sz) {
-                [, $pos, $sz] = $y->sz;
-                $y->sz = 0;
-                $this->array[$pos]->sz = $sz;
-            }
-            $this->array[$i] = clone $y;
+            $this->lines += $cur->nl;
         }
-        $this->array[0] = [$tokens->getReturn()];
+        //exit;
+    }
+
+    function tok($tok, $prev) {
+        return (object)[
+            'tok' => ($is = is_array($tok)) ? $tok[0] : 0,
+            'str' => $str = $is ? $tok[1] : $tok,
+            'nl' => substr_count($str, "\n"), # new lines count
+            'prev' => $prev,
+            'next' => null,
+            'sz' => 0,
+            'rank' => 0, # KEYWORD/CHARS/USAGE/DEFINITION
+            'oc' => 0, # open/none/close 1/0/-1
+        ];
     }
 }
