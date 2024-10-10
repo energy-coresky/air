40c227e1b545be97692623fa121af77e1d75024a
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-10-10 10:14:47 +0300

    tiny fix

--- w2/php.php
+++ w2/php.php
@@ -2,69 +2,84 @@
 
 class PHP
 {
-    const version = 0.303;
+    const version = 0.388;
 
     const KEYWORD = 1;
     const USAGE = 2;
     const DEFINITION = 3;
-    const CHARS = 4;
+    const IGNORE = 4;
     const TYPE = 5;
+    const CHAR = 6;
 
-    static $php;
-
-    //public $lines = 1;
     public $pad; # 0 for minified PHP
     public $tok;
     public $count = 0;
+    public $syntax_fail = false;
 
+    private $php;
     private $ns = '';
     private $use = [];
     private $stack = [];
-    private $sz = [];
+    private $x = [];
     private $oc = [
         '{' => '}',
         '(' => ')',
         '[' => ']',
     ];
 
-    function __construct(string $in = '', $pad = 0) {
+    function __construct(string $in, $pad = 1) {
         defined('T_ENUM') or define('T_ENUM', 11001);
-        self::$php or self::$php = yml('+ @object@inc(php)');
-        $this->tok = token_get_all(unl($in), TOKEN_PARSE);
+        $this->php = Plan::php();
+        try {
+            $this->tok = token_get_all(unl($in), TOKEN_PARSE);
+        } catch (Throwable $e) {
+            $this->tok = [$this->syntax_fail = $e->getMessage()];
+        }
         $this->count = count($this->tok);
-        //$this->lines = substr_count($this->in, "\n");
         $this->pad = $pad;
     }
 
-    static function file($name) {
-        echo new PHP(file_get_contents($name));
+    function __get($name) {
+        $name = substr($name, 1);
+        return $this->php->$name;
     }
 
-    function trace($t) {
-        static $i = 0; $i++;
-        $s = $t->tok . ' ' . token_name($t->tok) . ' ' . $t->str;
-        if ($t->sz)
-            $s .= " ------------------- SZ: $t->sz";
-        return "===================== $i\n$s\n";// . var_export($t, 1);
+    static function file($name, $pad = 4) {
+        return new PHP(file_get_contents($name), $pad);
     }
+
+ function trace($t) {
+     static $i = 0; $i++;
+     $s = $t->tok . ' ' . token_name($t->tok) . ' ' . $t->str;
+     if (!$t->x)
+         $s .= " ------------------- SZ: $t->x";
+     return "===================== $i\n$s\n";// . var_export($t, 1);
+ }
 
     function __toString() {
-        $this->parse_nice();
-        $out = $x = '';
+        $this->parse_rank();
+        $out = $trc = '';
         for ($y = $this->tok(); $y; $y = $next) {
             $next = $this->tok($y->i + 1);
-$x .= $this->trace($y);
+$trc .= $this->trace($y);
             if (!$this->pad) {
                 $prev = $this->tok($y->i - 1);
                 if (T_COMMENT == $y->tok
-#                    || T_WHITESPACE == $y->tok && in_array($prev->str, self::$php->prev_space)
-#                    || T_WHITESPACE == $y->tok && in_array($next->str, self::$php->after_space)
+#                    || T_WHITESPACE == $y->tok && in_array($prev->str, $this->_prev_space)
+#                    || T_WHITESPACE == $y->tok && in_array($next->str, $this->_after_space)
                 )
                     continue;
             }
             $out .= $y->str;
         }
-        return $out . "\nlines: ??\n$x";
+        return $out . "\nlines: ??\n$trc";
+    }
+
+    function char_line($i) {
+        for ($d = 1; $tok =& $this->tok[$i - $d++]; ) {
+            if ($tok[0])
+                return $tok[2] + substr_count($tok[1], "\n");
+        }
     }
 
     function tok($i = 0) {
@@ -76,14 +91,14 @@ $x .= $this->trace($y);
             'i' => $i,
             'tok' => $tok[0],
             'str' => &$tok[1],
-            'rank' => 0, # KEYWORD/CHARS/USAGE/DEFINITION
-            'sz' => $this->sz[$i] ?? 0,
-//            'oc' => 0, # open/none/close 1/0/-1
+            'line' => $tok[0] ? $tok[2] : fn() => $this->char_line($i),
+            'x' => $this->x[$i] ?? 0,
+            'oc' => 0, # open/none/close 1/0/-1
         ];
     }
 
     function parse_nice() {
-        $sz =& $this->sz;
+        $x =& $this->x;
         $stk =& $this->stack;
         $open = array_keys($this->oc);
         $close = array_values($this->oc);
@@ -96,30 +111,38 @@ $x .= $this->trace($y);
             }
             
             if ($stk)
-                $sz[end($stk)] += strlen(' ' == $y->str ? ' ' : trim($y->str));
+                $x[end($stk)] += strlen(' ' == $y->str ? ' ' : trim($y->str));
             if (in_array($y->str, $open)) {
                 $stk[] = $y->i;
-                $sz[$y->i] = 1;
+                $x[$y->i] = 1;
             } elseif (in_array($y->str, $close)) {
                 $char = $this->tok[end($stk)][1];
                 if ($y->str == $this->oc[$char]) { // checking!!
                     $j = array_pop($stk);
                     if ($stk)
-                        $sz[end($stk)] += $sz[$j] - 1;
+                        $x[end($stk)] += $x[$j] - 1;
                 }
             }
         }
     }
 
+    function match() {
+    }
+
     function parse_rank() {
-        $line = 1;
-        foreach ($this->tokens() as $i => $y) {
+        
+        for ($y = $this->tok(); $y; $y = $next) {
+            $next = $this->tok($y->i + 1);
             if (T_STRING == $y->tok) {
-                if (in_array($y->str, self::$php->types))
-                    $y->rank = PHP::TYPE;
+                if (in_array($y->str, $this->_types))
+                    $this->x[$y->i] = PHP::TYPE;
                     
-            } elseif (in_array($y->str, self::$php->keywords)) { // 2do: chk
-                $y->rank = PHP::KEYWORD;
+            } elseif (in_array($y->tok, [T_WHITESPACE, T_COMMENT, T_DOC_COMMENT])) {
+                $this->x[$y->i] = PHP::IGNORE;
+            } elseif (in_array($y->str, $this->_keywords)) { // 2do: chk
+                $this->x[$y->i] = PHP::KEYWORD;
+            } else {
+                $this->x[$y->i] = PHP::CHAR;
             }
             switch ($y->tok) {
                 case T_NAMESPACE:
@@ -132,7 +155,7 @@ $x .= $this->trace($y);
                 case T_TRAIT: ;
                 case T_EVAL: ;
             }
-            $line += substr_count($y->str, "\n");
+            
         }
     }
 }
